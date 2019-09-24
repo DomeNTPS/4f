1f5c0b595570c03d09699c009d98f37022ac5369
DomeNTPS <tanndome_555@hotmail.com>
2019-09-24 17:40:03 +0700

    ui update

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import { View,Text,StyleSheet,Image,style,Button,TextInput,useState } from 'react-native';
 import React,{Component} from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -11,16 +12,37 @@ export default class Login extends React.Component {
     render() {   
         
          return(
-        <View style = { styles.container }>
-            <View style = {{flex: 0.07 , width: 100,backgroundColor: 'orange',}}> 
-                <Text style = {{marginLeft: 27,marginTop: 15,fontSize: 20 }} >Login</Text>
-            </View>
-            <View style = {styles.blank}></View>
-            < TextInput
-             onChangeText = {
-                 (text) => this.setState({
-                     text
-                 })
+            <View style = { styles.container }> 
+                < View style = {styles.blank} ></View>
+                < View style = {
+                    {
+                        alignSelf: 'center',
+                        height: 150,
+                    }
+                } >
+                    <Image style = {
+                    {
+                        
+                        width: 150,
+                        height: 150
+                    }
+                }
+                    source = {
+                        require('../Image/tools.png')
+                    }
+         /></View> 
+        <View style = {styles.blank}></View>
+            
+        <Text style = {{paddingRight: 250,fontSize: 20,color: 'white'}} >Login</Text>
+
+       
+
+        <View style = {styles.bla}></View>
+        <TextInput
+            onChangeText = {
+             (text) => this.setState({
+                 text
+             })
              }
              value = {
                  this.state.text
@@ -29,25 +51,66 @@ export default class Login extends React.Component {
             style = {
                 {
                     height: 40,
-                    width: 200,
+                    width: 300,
                     borderColor: '#fff',
-                    borderWidth: 1
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    backgroundColor: '#fff',
+                   
                    
                 }
             }
             />
+
+            <View style = {{flex: 0.01}}></View>
+
+            <TextInput
+            onChangeText = {
+                (text2) => this.setState({
+                    text2
+                })
+            }
+            value2 = {
+                this.state.text2
+            }
+            placeholder = "Password"
+            style = {
+                {
+                    alignContent: 'center',
+                    height: 40,
+                    width: 300,
+                    borderColor: '#fff',
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    backgroundColor: '#fff',
+                }
+            }
+            />
             <View style = { styles.bla } ></View>
-            < Button
-            title = "Submit"
-            color='orange'
-            alignItems='center'
-            backgroundColor='#000'
-            onPress = {
-                    () =>
-                    this.props.navigation.navigate('Home')
-                } >
-                </Button>
+
+            <TouchableOpacity style = {styles.container2}
+                 onPress = {
+                     () =>
+                     this.props.navigation.navigate('Home')
+                }
+                underlayColor = '#fff'>
+                            <Text style = {
+                            styles.loginText
+                        }> Submit </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+                onPress = {
+                     () => 
+                     this.props.navigation.navigate('Home')
+                }
+                underlayColor = '#fff' >
+                            <Text style = {
+                            styles.GuestText
+                        } > Guest Login </Text> 
+            </TouchableOpacity>
         </View>
+    
         );
     }
 
@@ -56,17 +119,46 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'grey',
+        backgroundColor: '#000',
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
+    },
+
+    container2: {
+        backgroundColor: 'orange',
+        alignItems: 'center',
+        //justifyContent: 'center',
+        borderColor: 'white',
+        borderRadius: 10,
+    },
+
+    loginText: {
+        color: '#fff',
+        textAlign: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 20,
+    },
+
+    GuestText: {
+        color: '#fff',
+        textAlign: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 20,
     },
 
         bla:{
-            flex: 0.2,
+            flex: 0.03,
+            alignItems: 'center',
         },
         blank:{
-            flex: 0.11,
-        }
+            flex: 0.2,
+            alignItems: 'center',
+        },
+        space:{ flex: 0.05,},
+
+    
     
 
 });
