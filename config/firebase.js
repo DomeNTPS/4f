ac1dab68fd5b44ca921ce27da606adb112e0ddf6
Nontakon <59010679@kmitl.ac.th>
2019-09-26 23:11:25 +0700

    qr scanner
    qr scanner

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB989tkkzWoY9DfOHOqkwckJdNawhaoAdg",
+    authDomain: "project4f-6c9ee.firebaseapp.com",
+    databaseURL: "https://project4f-6c9ee.firebaseio.com",
+    projectId: "project4f-6c9ee",
+    storageBucket: "",
+    messagingSenderId: "642478986778"
+};
+firebase.initializeApp(config);
+
+export default firebase;
